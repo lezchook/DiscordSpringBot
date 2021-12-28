71dd59d0312d7e0c28b749a49c8160005c30a3ba
Sergei Leshchenko <  >
2021-12-29 00:38:48 +0300

    Final version

--- src/main/java/com/leshchenko/starcraftbot/command/AddPlayer.java
+++ src/main/java/com/leshchenko/starcraftbot/command/AddPlayer.java
@@ -1,21 +1,9 @@
 package com.leshchenko.starcraftbot.command;
 
 import com.leshchenko.starcraftbot.domain.User;
-import com.leshchenko.starcraftbot.repo.UserRepo;
 import com.leshchenko.starcraftbot.service.DataBaseService;
-import net.dv8tion.jda.api.events.application.ApplicationCommandCreateEvent;
-import net.dv8tion.jda.api.events.application.GenericApplicationCommandEvent;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
-import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import org.jetbrains.annotations.NotNull;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.jws.soap.SOAPBinding;
 import java.util.Iterator;
 import java.util.List;
 
--- src/main/java/com/leshchenko/starcraftbot/repo/UserRepo.java
+++ src/main/java/com/leshchenko/starcraftbot/repo/UserRepo.java
@@ -2,7 +2,6 @@ package com.leshchenko.starcraftbot.repo;
 
 import com.leshchenko.starcraftbot.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
